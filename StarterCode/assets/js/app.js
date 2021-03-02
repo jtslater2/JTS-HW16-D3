939e1d8d2690c3248bb10e8c1fbd3da41d0ce155
unknown <jtslater2@gmail.com>
2021-03-02 16:00:09 -0500

    uggh

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -2,26 +2,45 @@
 //var dataArray = [1, 2, 3];
 //var dataCategories = ["one", "two", "three"];
 
-function makeResponsive() {
+// function makeResponsive() {
 
     // if the SVG area isn't empty when the browser loads,
     // remove it and replace it with a resized version of the chart
-  var svgArea = d3.select("body").select("svg");
 
-  if (!svgArea.empty()) {
-    svgArea.remove();
-  }
+  // var svgHeight = 500;
+  // var svgWidth = 1000;
+  
+  // var svgArea = d3.select("body").append("svg")
+  //     .attr("height", svgHeight)
+  //     .attr("width", svgWidth);
+  
+
+  // if (!svgArea.empty()) {
+  //   svgArea.remove();
+  // }
+
+  var svgHeight = 500;
+  var svgWidth = 1000;
+  
+  var svgArea = d3.select("body").append("svg")
+       .attr("height", svgHeight)
+       .attr("width", svgWidth);
+
+
 
     // svg params
-  var svgHeight = window.innerHeight;
-  var svgWidth = window.innerWidth;
+  // var svgHeight = window.innerHeight;
+  // var svgWidth = window.innerWidth;
+  // var svgHeight = 500;
+  // var svgWidth = 1000;
 
-    // margins
+
+    // margin
   var margin = {
-    top: 50,
-    right: 50,
-    bottom: 50,
-    left: 50
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 10
   };
 
     // chart area minus margins
@@ -29,9 +48,9 @@ function makeResponsive() {
   var chartWidth = svgWidth - margin.left - margin.right;
 
     // create svg container
-  var svg = d3.select("body").append("svg")
-        .attr("height", svgHeight)
-        .attr("width", svgWidth);
+  // var svg = d3.select("body").append("svg")
+  //        .attr("height", svgHeight)
+  //        .attr("width", svgWidth);
 
     // shift everything over by the margins
   var scatterGroup = svg.append("g")
@@ -63,7 +82,7 @@ function makeResponsive() {
 
 
   //Load data csv
-  d3.csv("assets/data/data.csv").then(function(scatter_data) {
+d3.csv("assets/data/data.csv").then(function(scatter_data) {
     console.log (scatter_data) 
     
     scatter_data.forEach(function(data) {
@@ -77,8 +96,14 @@ function makeResponsive() {
         .data(scatter_data)
         .enter()
         .append("circle")
-        .class
-  
+        .classed("scatter", true)
+        .attr("cx", d => d.age)
+        .attr("cy", d => d.smokesHigh)
+        .attr("r", 2)
+        .attr("stroke", "blue")
+        .attr("stroke-width", "1")
+        .attr("fill", "grey")
+        .attr("text", d => d.abbr);
   
   
   
@@ -113,12 +138,12 @@ function makeResponsive() {
 
 
 
-}
+// }
 
-makeResponsive();
+// makeResponsive();
 
 // Event listener for window resize.
 // When the browser window is resized, makeResponsive() is called.
-d3.select(window).on("resize", makeResponsive);
+// d3.select(window).on("resize", makeResponsive);
 
 
