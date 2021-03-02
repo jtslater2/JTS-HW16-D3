31c6b0babf3f3cccf23234b4f76ae8d901fef79c
unknown <jtslater2@gmail.com>
2021-03-02 14:43:57 -0500

    updates

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,6 +1,6 @@
 // @TODO: YOUR CODE HERE!
-var dataArray = [1, 2, 3];
-var dataCategories = ["one", "two", "three"];
+//var dataArray = [1, 2, 3];
+//var dataCategories = ["one", "two", "three"];
 
 function makeResponsive() {
 
@@ -34,57 +34,62 @@ function makeResponsive() {
         .attr("width", svgWidth);
 
     // shift everything over by the margins
-  var chartGroup = svg.append("g")
+  var scatterGroup = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    // scale y to chart height
-  var yScale = d3.scaleLinear()
-        .domain([0, d3.max(dataArray)])
-        .range([chartHeight, 0]);
+  //   // scale y to chart height
+  // var yScale = d3.scaleLinear()
+  //       .domain([0, d3.max(dataArray)])
+  //       .range([chartHeight, 0]);
 
-    // scale x to chart width
-  var xScale = d3.scaleBand()
-        .domain(dataCategories)
-        .range([0, chartWidth])
-        .padding(0.1);
+  //   // scale x to chart width
+  // var xScale = d3.scaleBand()
+  //       .domain(dataCategories)
+  //       .range([0, chartWidth])
+  //       .padding(0.1);
 
-    // create axes
-  var yAxis = d3.axisLeft(yScale);
-  var xAxis = d3.axisBottom(xScale);
+  //   // create axes
+  // var yAxis = d3.axisLeft(yScale);
+  // var xAxis = d3.axisBottom(xScale);
 
-    // set x to the bottom of the chart
-  chartGroup.append("g")
-        .attr("transform", `translate(0, ${chartHeight})`)
-        .call(xAxis);
+  //   // set x to the bottom of the chart
+  // chartGroup.append("g")
+  //       .attr("transform", `translate(0, ${chartHeight})`)
+  //       .call(xAxis);
 
-    // set y to the y axis
-  chartGroup.append("g")
-        .call(yAxis);
+  //   // set y to the y axis
+  // chartGroup.append("g")
+  //       .call(yAxis);
 
 
-  chartGroup.selectAll("rect")
-        .data(dataArray)
-        .enter()
-        .append("rect")
-        .attr("x", (d, i) => xScale(dataCategories[i]))
-        .attr("y", d => yScale(d))
-        .attr("width", xScale.bandwidth())
-        .attr("height", d => chartHeight - yScale(d))
-        .attr("fill", "green")
-        // event listener for onclick event
-        .on("click", function(d, i) {
-          alert(`Hey! You clicked bar ${dataCategories[i]}!`);
-        })
-        // event listener for mouseover
-        .on("mouseover", function() {
-          d3.select(this)
-                .attr("fill", "red");
-        })
-        // event listener for mouseout
-        .on("mouseout", function() {
-          d3.select(this)
-                .attr("fill", "green");
-        });
+  //Load data csv
+  d3.csv("assets/data/data.csv").then(function(scatter_data) {
+    console.log (scatter_data) });
+  
+  
+    // chartGroup.selectAll("rect")
+    //     .data(dataArray)
+    //     .enter()
+    //     .append("rect")
+    //     .attr("x", (d, i) => xScale(dataCategories[i]))
+    //     .attr("y", d => yScale(d))
+    //     .attr("width", xScale.bandwidth())
+    //     .attr("height", d => chartHeight - yScale(d))
+    //     .attr("fill", "green")
+    //     // event listener for onclick event
+    //     .on("click", function(d, i) {
+    //       alert(`Hey! You clicked bar ${dataCategories[i]}!`);
+    //     })
+    //     // event listener for mouseover
+    //     .on("mouseover", function() {
+    //       d3.select(this)
+    //             .attr("fill", "red");
+    //     })
+    //     // event listener for mouseout
+    //     .on("mouseout", function() {
+    //       d3.select(this)
+    //             .attr("fill", "green");
+    //     });
 }
 
 makeResponsive();
